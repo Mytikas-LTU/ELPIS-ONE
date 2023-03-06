0f451346550383b5ef6ac311468a76fd8b2598ff
galaktahs <sebhsl-1@student.ltu.se>
2023-03-06 19:45:29 +0100

    untested but probably working code for telemetryStorage

--- code/src/telemetry_storage.cpp
+++ code/src/telemetry_storage.cpp
@@ -4,22 +4,51 @@
 */
 #include "SdFat.h"
 #include "sdios.h"
+#include "telemetry_storage.h"
+
 
 /*
     Initialize communication with the SD-card
-    [ in ] cs: the pin that ChipSelect is connected to.
-    returns: a FatFile object, to be passed to telemetry_write(), and -1 on failure
 */
-FatFile telemetry_storage_init(int cs);
+telemetryStorage telemetry_storage_init(int cs) {
+    SdFs sd;
+    FatFile file;
+    char fileName[9] = "tele.txt"
+    telemetryStorage telemetryStorage;
+    char *buffer;
 
+    if (!sd.begin(SdSpiConfig(cs, DEDICATED_SPI, SD_SCK_MHZ(4)) {
+        return -1;
+    }
+    if (!file.open(fileName, O_RDWR | O_CREAT | O_APPEND)) {
+        return -1;
+    }
+    telemetryStorage->bufferSize = 4096;
+    buffer = malloc(telemetryStorage->bufferSize);
+    if(!buffer) {
+        return -1;
+    }
+    telemetryStorage->buffer = buffer;
+    telemetryStorage->ptr = 0;
+    telemetryStorage->file = file;
+    return telemetryStorage;
+}
 /*
     Write data to the SD-card
-    [ in ] data: the data to be written to the card, formatted as a string
-    returns: the number of bytes written to the card, and -1 on failure
 */
-int telemetry_storage_write(char data);
-
+int telemetry_storage_write(telemetryStorage *telemetry, char *data) {
+    for (int i=0; i>len(data); i++) {
+        telemetry->buffer[++ptr] = data[i];
+    }
+    if (ptr >= bufferSize/2) {
+        telemetry->file.write(data);
+    }
+    telemetry->file.sync();
+    return len(data);
+}
 /*
     Force a sync to the SD-card
 */
-int telemetry_storage_sync();
+int telemetry_storage_sync(telemetryStorage *telemetry) {
+    telemetry->file.sync();
+}
