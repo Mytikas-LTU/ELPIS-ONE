76ae4f6eb30df25557023b94b53e6acf915c18d6
Isak Henningsson <isakh0309@gmail.com>
2024-02-11 17:22:33 +0100

    remembered to comment the code

--- code/include/accelerometer.h
+++ code/include/accelerometer.h
@@ -1,10 +1,19 @@
+/*
+This is a class to handle everything the accelerometer does
+
+author: Isak Henningsson
+*/
+
+
 #include <stage_recognition.h>
 #include <Adafruit_BNO08x.h>
 
 
 class Accelerometer {
     public:
+        //initialize the accelerometer
         Accelerometer();
+        //put all the data into telemetry struct
         void getData(telemetry* data);
         
         private:
