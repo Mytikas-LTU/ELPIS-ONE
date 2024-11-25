743e094ca95c296bcc8119e86f058037927be423
galaktahs <sebhsl-1@student.ltu.se>
2024-11-25 13:06:38 +0100

    inital work on dummydata, frameworks etc.

---
+++ code/include/Dummydata.h
@@ -0,0 +1,20 @@
+/*
+    Generation of Dummy data, for testing purposes
+
+    author: Aurora Å/
+*/
+#ifndef __DUMMYDATA__
+#define __DUMMYDATA__
+
+#include<stage_recognition.h>
+
+
+class Dummydata{
+    public:
+        Dummydata();
+        void getData(telemetry* tel);
+//    private:
+
+};
+
+#endif
